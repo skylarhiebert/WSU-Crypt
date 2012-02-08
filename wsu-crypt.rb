079a2b86698bd776051c01909610c859d8822032
Skylar Hiebert <skylar.hiebert@computer.org>
2012-02-07 22:19:55 -0800

    Completed Key Functions

--- wsu-crypt.rb
+++ wsu-crypt.rb
@@ -38,7 +38,7 @@ def generate_encryption_keys(key)
 	
 	0.upto(191) do |index|
 		kp = left_shift(kp) # left rotate
-		skeys = get_num_bits(kp, 8)
+		skeys = get_num_bits(kp, 8) # .reverse for other subkey schedule
 		byte = (4 * (index / 12) + (index % 4)) % 8
 		keys[index] = skeys[byte]
 	end
@@ -50,9 +50,12 @@ def generate_decryption_keys(key)
 	kp = key
 
 	0.upto(191) do |index|
-		skeys = get_num_bits(kp, 8)
+		skeys = get_num_bits(kp, 8).reverse # Opposite of encryption
 		byte = (4 * (index / 12) + (index % 4)) % 8
-		keys[index] = skeys[byte]
+		# Slot is to reverse 12 keys (11 downto 0, then 23 downto 12)
+		slot = (12 * (index/12 + 1) - 1) - index % 12
+		# Reverse Key Ordering, 
+		keys[slot] = skeys[byte] 
 	   kp = right_shift(kp)
 	end
 	return keys
@@ -65,24 +68,48 @@ def whiten (text, key)
 	0.upto(3) do |i|
 		rvals[i] = (words[i].to_i(2) ^ wkeys[i].to_i(2)).to_s(2)
 	end
-	p rvals
+	return rvals
 end
+
+def F (r0, r1, round)
+		
+end
+
+def G (r0, round)
+	g = Array.new
+	g[1] = r0[0, 8]
+	g[2] = r0[8, 8]
+	p g
+end
+
+# str.pack('B*')[0] converts string to binary string
+# bstr.pack('B*') converts binary string to ascii
+# hstr.pack('B*').unpack('H*')[0] converts binary to hex characters
 
 key = "abcdef0123456789"
 plaintext = "0123456789abcdef"
-bkey = hex_to_binary(key)
+bkey = hex_to_binary(key) # Use for debugging
+#bkey = key.unpack('B64') # Use for generic code
 bpt = hex_to_binary(plaintext)
 
-keys = Array.new
+ekeys = Array.new
+dkeys = Array.new
 kbytes = get_num_bits(bkey, 8)
+ekeys = generate_encryption_keys(bkey)
+dkeys = generate_decryption_keys(bkey)
 
-keys = generate_encryption_keys(bkey)
+#puts dkeys#.pack("C*")
 
+# Print Key Table for debugging
 0.upto(15) do |i|
-	0.upto(11) do |j|	
-		print "#{keys[i*12 + j].to_i(2).to_s(16)}\t" 
+	0.upto(11) do |j|
+		index = i*12 + j
+		print "#{ekeys[index].to_i(2).to_s(16)}:#{dkeys[index].to_i(2).to_s(16)}\t"
 	end
 	print "\n"
 end
 
-whiten(bpt, bkey)
+rvals = whiten(bpt, bkey)
+p binary_to_hex(rvals.join)
+round = 0
+G(rvals[0], round)
